51bec5909620d20f9a03b8255f9a32d5a9a81270
baruc sanchez <barucgs@gmail.com>
2025-09-18 18:34:46 -0600

    proyect template basic

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+    require("tailwindcss-animate"),
+    // Plugins opcionales:
+    // require("tailwindcss-elevation"),
+    // require("tailwindcss-fluid-type"),
+  ],
+};
